c4fab69da458030921ea00b87172eb0fb0f43b8d
disk6121 <disk28rstdm3@yahoo.co.jp>
2026-07-08 08:46:34 +0900

    Update chart_utils.py

--- utils/chart_utils.py
+++ utils/chart_utils.py
@@ -1,17 +1,9 @@
 
-from matplotlib import font_manager
-import matplotlib.pyplot as plt
-from pathlib import Path
 
 
 
 def draw_growth_arrow(ax, df, target_idx, compare_idx, label, color, offset_y):
-    font_path = Path(__file__).resolve().parent.parent / "fonts" / "NotoSansJP-VariableFont_wght.ttf"
 
-    font_manager.fontManager.addfont(str(font_path))
-
-    plt.rcParams["font.family"] = "Noto Sans JP"
-    plt.rcParams["axes.unicode_minus"] = False
 
     if target_idx < len(df) and compare_idx >= 0 and compare_idx < len(df):
         val_target = df.iloc[target_idx]['Revenue']
