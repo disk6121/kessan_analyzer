6c61932528014c1cd6b6f32cd061b579b468bbc4
disk6121 <disk28rstdm3@yahoo.co.jp>
2026-07-11 23:27:29 +0900

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -390,14 +390,25 @@ if st.session_state.get("current_analysis"):
     st.divider()
     st.write("#### 💾 この企業のデータをウォッチリストに保存しますか？")
 
-    if st.button("🌟 この分析結果をデータベースに保存する", type="primary", use_container_width=True):
 
-        save_analysis_data(
-            analysis,
-            st.session_state.reports_dict,
-            st.session_state.deep_dive_memo_input,
-            st.session_state.peer_comparison_df
-        )
+    if st.button(...):
+    st.write("A")
+    save_analysis_data(...)
+    st.write("B")
+    st.success("保存しました")
+    # st.rerun()
+
+
+
+    
+    #if st.button("🌟 この分析結果をデータベースに保存する", type="primary", use_container_width=True):
+
+     #   save_analysis_data(
+      #      analysis,
+       #     st.session_state.reports_dict,
+        #    st.session_state.deep_dive_memo_input,
+         #   st.session_state.peer_comparison_df
+        #)
  
-        st.success(f"🎉 {analysis['meta']['company_name']} のデータを【統合保存】しました！")
-        st.rerun() 
+ #       st.success(f"🎉 {analysis['meta']['company_name']} のデータを【統合保存】しました！")
+  #      st.rerun() 
