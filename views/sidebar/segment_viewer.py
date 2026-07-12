99f85ecf28238c133bc469e73c0b387900f53779
disk6121 <disk28rstdm3@yahoo.co.jp>
2026-07-12 23:03:47 +0900

    Update segment_viewer.py

--- views/sidebar/segment_viewer.py
+++ views/sidebar/segment_viewer.py
@@ -6,7 +6,7 @@ def render_segment_viewer(df_pivot_rev, df_pivot_prof):
 
     with st.expander("🔍 セグメント別単四半期売上・利益"):
         st.write("##### 🔹 セグメント別 単四半期売上高 (百万円)")
-        st.dataframe(df_pivot_rev, use_container_width=True)
+        st.dataframe(df_pivot_rev, width="stretch")
         st.write("##### 🔹 セグメント別 単四半期利益 (百万円)")
-        st.dataframe(df_pivot_prof, use_container_width=True)
+        st.dataframe(df_pivot_prof, width="stretch")
 
