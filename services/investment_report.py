4e694476d3f7fe1e35abf5b623204e4080da7d70
disk6121 <disk28rstdm3@yahoo.co.jp>
2026-07-19 14:17:00 +0900

    Update investment_report.py

--- services/investment_report.py
+++ services/investment_report.py
@@ -7,17 +7,17 @@ from database.load_repository import load_analysis_data
 
 def generate_investment_report(api_key,ticker):
 
-    row_data, meta_row = load_analysis_data(ticker)
+    row_data, meta_row = load_analysis_data(ticker)   
     
-    combined_data = 
-    annual_performance,
-    financial_meta,
-    segment_analysis,
-    peer_comparison,
-    deep_dive_reports,
-    kpi
-    
-
+    combined_data = load_json(row_data["combined_data_json"]
+    annual_performance = load_json(meta_row["annual_perf_json"]) 
+    financial_meta = load_json(meta_row["financial_meta_json"]) 
+    segment_analysis = load_json(row_data["seg_data_json"])
+    if row_data["peer_comparison_json"]:    
+        peer_comparison = load_json(row_data["peer_comparison_json"])
+    deep_dive_reports = row_data["deep_dive_memo"]
+    if meta_row.get("kpi_json"):
+        kpi = load_json(meta_row["kpi_json"])
     
     client = genai.Client(api_key=api_key)
 
