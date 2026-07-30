63025433a18a5e2bafc3745a3a29fed076121897
disk6121 <disk28rstdm3@yahoo.co.jp>
2026-07-31 06:06:44 +0900

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -84,13 +84,14 @@ if not df_db.empty:###　companiesテーブルの項目を日本語名に置き�
             "buy_target", 
             "sell_target",
             "alert_status",
+            "expectation",
             "announcement_display",
             "announcement_type"
         ]
     ].rename(columns={
         "ticker": "証券コード", "company_name": "企業名", "saved_date": "保存日", "current_price": "株価",
         "investment_memo": "投資メモ", "is_favorite": "⭐お気に入り", 
-        "buy_target": "買いたい価格", "sell_target": "売りたい価格", "alert_status":"アラート",
+        "buy_target": "買いたい価格", "sell_target": "売りたい価格", "alert_status":"アラート","expectation": "決算期態度(%)"
         "announcement_display": "決算予定", "announcement_type": "決算種別"
     })
 
@@ -100,7 +101,7 @@ if not df_db.empty:###　companiesテーブルの項目を日本語名に置き�
             "⭐お気に入り": st.column_config.CheckboxColumn(help="気になる銘柄をチェック"),
             "投資メモ":st.column_config.TextColumn(width="large"),
             },
-        disabled=["証券コード", "企業名", "保存日", "株価", "決算予定", "決算種別"],
+        disabled=["証券コード", "企業名", "保存日", "株価", "決算期態度(%)", "決算予定", "決算種別"],
         width="stretch",
         key="editor"
     )
